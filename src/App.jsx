d0a77f2eb4c5d0a2feb8b87c2ff2f51d6174b54f
phoebe <phoebegogh@163.com>
2022-05-26 23:34:23 +0800

    Add Homepage

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import List from './components/List'
+import Footer from './components/Footer'
+
+import './App.css'
+
+export default class App extends Component {
+
+  state = {todos: [
+    {id:'001',name:'吃饭',done:true},
+    {id:'002',name:'睡觉',done:true},
+    {id:'003',name:'打豆豆',done:false},
+    {id:'003',name:'啪啪',done:false},
+  ]}
+  render() {
+    const {todos} = this.state
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header />
+          <List todos={todos}/>
+          <Footer/>
+      </div>
+    </div>
+    )
+  }
+}
