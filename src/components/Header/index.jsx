1f478c118d15a0355c0cab35d95faafd642f8264
phoebe <phoebegogh@163.com>
2022-05-27 23:05:23 +0800

    Add Input

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {nanoid} from 'nanoid'
 import './index.css'
 
 export default class Header extends Component {
 
+  static propTypes = {
+    addTodo:PropTypes.func.isRequired
+  }
+
   handleKeyUp = (event)=>{
-    if(event.keyCode !==13) return
-    console.log(event.target.value);
+    const {keyCode,target} = event
+    if(keyCode !==13) return
+    if(target.value.trim() === ''){
+      alert('输入不能为空')
+      return
+    }
+    const todoObj = {id:nanoid(),name:target.value,done:false}
+    this.props.addTodo(todoObj)
+    target.value = ''
   }
 
   render() {
